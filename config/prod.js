b4112013afa7575c30807dde4dca44f45ea74576
Ongki Herlambang <meepomodeon@gmail.com>
2019-03-05 17:09:08 +0700

    refactor mongoose connection

--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,5 @@ module.exports = {
     channelSecret: process.env.CHANNEL_SECRET,
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   },
-  url: process.env.HOST_NAME,
+  url: process.env.HOSTNAME,
 }
