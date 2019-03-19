55ef512c23237fb6c7be7d2f43129095112efdf6
Ongki Herlambang <meepomodeon@gmail.com>
2019-03-20 01:34:18 +0700

    add new method for account setup

--- linebot/postback/approveDispatcher.js
+++ linebot/postback/approveDispatcher.js
@@ -1,12 +1,10 @@
-const reportStillPending = require('../action/reportStillPending')
-const locationAction = require('../action/locationAction')
-const templateFormat = require('../action/templateFormat')
+const config = require('./../../config')
 
 const User = require('./../../models/User')
 
-module.exports = async (data, event, bot) => {
+const templateFormat = require('../action/templateFormat')
 
-  console.log("Approving...")
+module.exports = async (data, event, bot) => {
 
   let id = data.userId
   let user = await User.findById(id)
@@ -20,15 +18,40 @@ module.exports = async (data, event, bot) => {
       user.role = 'dispatcher'
       user.requestRole = {role: null, status: null}
       await user.save()
-      let formatEmail = templateFormat("Untuk mengatur email akunmu", "EMAIL:[spasi]contoh@email.com")
-      let formatPassword = templateFormat("Untuk mengatur password akunmu", "PWD:[spasi]password_akunmu")
+      // let formatEmail = templateFormat("Untuk mengatur email akunmu", "EMAIL:[spasi]contoh@email.com")
+      // let formatPassword = templateFormat("Untuk mengatur password akunmu", "PWD:[spasi]password_akunmu")
+
+      let setupAccount = {
+        "type": "template",
+        "altText": "Pengaturan akun admin",
+        "template": {
+          "type": "buttons",
+          "actions": [
+            {
+              "type": "uri",
+              "label": "PENGATURAN AKUN",
+              "uri": `${config.webUrl}/auth/:userid`
+            }
+          ],
+          "thumbnailImageUrl": `${config.url}/images/icons/mail.jpg`,
+          "title": "Pengaturan Akun",
+          "text": "Silahkan atur akun admin anda melalui link berikut"
+        }
+      }
+
+      console.log("Setup: ", setupAccount)
 
       bot.push(user.lineId, [
         `Selamat, kamu sekarang seorang ${user.role}`,
-        "Silahkan atur email dan password akunmu dengan mengirim pesan sesuai format berikut:",
-        formatEmail,
-        formatPassword,
+        setupAccount,
       ])
+
+      // bot.push(user.lineId, [
+      //   `Selamat, kamu sekarang seorang ${user.role}`,
+      //   "Silahkan atur email dan password akunmu dengan mengirim pesan sesuai format berikut pada link berikut:",
+      //   formatEmail,
+      //   formatPassword,
+      // ])
 
       return event.reply(['Approval berhasil'])
     }
--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const mongoose = require('mongoose')
-const pick = require('lodash.pick')
 
 const config = require('../config')
 
-const Report = require('../models/Report')
 const User = require('../models/User')
-const Broadcast = require('../models/Broadcast')
 
 const io = require('../services/socketClient')
 const bot = require('../services/linebot')
 
 const auth = require('./../middlewares/authentication')
-
-const headerAction = require('../linebot/action/reportHeaderAction')
-const confirmAction = require('../linebot/action/missionCompleted')
-const imageAction = require('../linebot/action/reportImageAction')
 
 router.post('/', async (req, res) => {
   try {
@@ -41,7 +33,6 @@ router.post('/me', auth, async (req, res) => {
 router.post('/logout', auth, async (req, res) => {
   try {
     let user = req.user
-    console.log("USER: ", user)
     user.tokens = user.tokens.filter(token => token.token !== req.token)
     let updatedUser = await user.save()
     res.status(200).send(updatedUser)
