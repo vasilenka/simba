31fccffc2639719f0ab8a57709e7e921e3d26eba
Ongki Herlambang <meepomodeon@gmail.com>
2019-03-19 23:27:03 +0700

    force https in production

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
 require('dotenv').config()
 
 const bot = require('./services/linebot')
@@ -14,6 +15,14 @@ const app = express()
 // Linebot
 app.post('/linewebhook', linebotParser)
 require('./linebot/__index')(bot)
+
+// Force HTTPS on production
+if (app.get('env') === 'production') {
+  app.use(function(req, res, next) {
+    let protocol = req.get('x-forwarded-proto')
+    protocol == 'https' ? next() : res.redirect('https://' + req.hostname + req.url)
+  })
+}
 
 const corsOptions = {
   credentials: true
@@ -28,7 +37,5 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/auth', require('./routes/auth'))
 require('./routes/__index')(app)
-
-console.log("HOSTNAME: ", process.env.MONGO_HOST)
 
 module.exports = app
