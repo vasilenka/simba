2ae90762434bdc3a1dc43bffa486869842f35e61
Ongki Herlambang <meepomodeon@gmail.com>
2019-03-27 15:38:33 +0700

    fix resetTokenVerification

--- middlewares/verifyResetToken.js
+++ middlewares/verifyResetToken.js
@@ -5,7 +5,7 @@ module.exports = async (req, res, next) => {
 
     let token = req.header('Authorization').replace('Bearer ', '')
     let decoded = jwt.verify(token, process.env.SECRET_TOKEN)
-    let user = await User.findOne({_id: decoded._id, 'tokens.token': token})
+    let user = await User.findById({_id: decoded._id})
 
     if(!user) {
       return res.status(401).json({error: 'Please authenticate'})
